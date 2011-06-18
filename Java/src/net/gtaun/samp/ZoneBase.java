7c39a13bd42734a5d4b5ac283e365d567b7ac77d
JingLin Wang <jojllman@gmail.com>
2011-06-19 04:13:07 +0800

    實現ZoneBase的主要功能:show, hide, flash, stopflash. 亦加入setColor, setFlashColor用以改變儲存的顏色
    Signed-off-by: JingLin Wang <jojllman@gmail.com>

--- Java/src/net/gtaun/samp/ZoneBase.java
+++ Java/src/net/gtaun/samp/ZoneBase.java
@@ -1,5 +1,6 @@
 /**
  * Copyright (C) 2011 MK124
+ * Copyright (C) 2011 JoJLlmAn
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,7 +22,7 @@ import java.util.Vector;
 import net.gtaun.samp.data.Area;
 
 /**
- * @author MK124
+ * @author MK124, JoJLlmAn
  *
  */
 
@@ -32,9 +33,18 @@ public class ZoneBase
 		return GameModeBase.getInstances( GameModeBase.instance.zonePool, cls );
 	}
 	
+	private boolean[] showedPlayer = new boolean[500];
+	private boolean[] flashedPlayer = new boolean[500];
 	
 	public int id;
+	public int color;
+	public int flashcolor;
 	public Area area;
+	
+	public int id(){return this.id;}
+	public int color(){return this.color;}
+	public int flashcolor(){return this.flashcolor;}
+	public Area area(){return this.area;}
 
 
 	public ZoneBase( float minx, float miny, float maxx, float maxy )
@@ -54,11 +64,109 @@ public class ZoneBase
 	private void init()
 	{
 		id = NativeFunction.gangZoneCreate( area.minX, area.minY, area.maxX, area.maxY );
+		
+		for(int i=0;i<500;i++){
+			showedPlayer[i] = false;
+			flashedPlayer[i] = false;
+		}
 	}
+	
+	//---------------------------------
 	
 	public void destroy()
 	{
 		NativeFunction.gangZoneDestroy( id );
 		GameModeBase.instance.zonePool[ id ] = null;
+	}
+	
+	public void show()
+	{
+		NativeFunction.gangZoneShowForAll(id, color);
+		
+		for(int i=0;i<500;i++)
+			showedPlayer[i] = true;
+	}
+	
+	public void show(PlayerBase player)
+	{
+		if(!showedPlayer[player.id]){
+			NativeFunction.gangZoneShowForPlayer(player.id, id, color);
+			showedPlayer[player.id] = true;
+		}
+	}
+	
+	public void hide()
+	{
+		NativeFunction.gangZoneHideForAll(id);
+		
+		for(int i=0;i<500;i++){
+			showedPlayer[i] = false;
+			flashedPlayer[i] = false;
+		}
+	}
+	
+	public void hide(PlayerBase player)
+	{
+		NativeFunction.gangZoneHideForPlayer(player.id, id);
+		showedPlayer[player.id] = false;
+		flashedPlayer[player.id] = false;
+	}
+	
+	public void flash()
+	{
+		NativeFunction.gangZoneFlashForAll(id, flashcolor);
+		for(int i=0;i<500;i++)
+		{
+			if(showedPlayer[i])
+				flashedPlayer[i] = true;
+		}
+	}
+	
+	public void flash(PlayerBase player)
+	{
+		if(showedPlayer[player.id])
+		{
+			NativeFunction.gangZoneFlashForPlayer(player.id, id, flashcolor);
+			flashedPlayer[player.id] = true;
+		}
+	}
+	
+	public void stopFlash()
+	{
+		NativeFunction.gangZoneStopFlashForAll(id);
+		for(int i=0;i<500;i++)
+			flashedPlayer[i] = false;
+	}
+	
+	public void stopFlash(PlayerBase player)
+	{
+		NativeFunction.gangZoneStopFlashForPlayer(player.id, id);
+		flashedPlayer[player.id] = false;
+	}
+	
+	public void setColor(int color)
+	{
+		this.color = color;
+		
+		for(int i=0;i<500;i++)
+		{
+			if(showedPlayer[i])
+			{
+				NativeFunction.gangZoneHideForPlayer(i, id);
+				NativeFunction.gangZoneShowForPlayer(i, id, color);
+				if(flashedPlayer[i])
+					NativeFunction.gangZoneFlashForPlayer(i, id, flashcolor);
+			}
+		}
+	}
+	
+	public void setFlashColor(int flashcolor)
+	{
+		this.flashcolor = flashcolor;
+		for(int i=0;i<500;i++)
+		{
+			if(flashedPlayer[i])
+				NativeFunction.gangZoneFlashForPlayer(i, id, flashcolor);
+		}
 	}
 }
