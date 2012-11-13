8b15fe0928099fe25284eca33151682c1e535f25
mk124 <mk124a@gmail.com>
2012-11-13 11:56:23 +0800

    Organize import.
    Signed-off-by: mk124 <mk124a@gmail.com>

--- src/main/java/net/gtaun/shoebill/SampObjectStoreImpl.java
+++ src/main/java/net/gtaun/shoebill/SampObjectStoreImpl.java
@@ -22,7 +22,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
